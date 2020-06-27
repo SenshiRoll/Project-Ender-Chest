9998e6a36cddbccb03203bdb801991c17411a784
SenshiRoll <64864318+SenshiRoll@users.noreply.github.com>
2020-06-27 04:29:46 -0400

    Delete NetworkConnection.java

--- NetworkConnection.java
+++
@@ -1,9 +0,0 @@
-public abstract class NetworkConnection implements Runnable {
-    
-    abstract void recievePackage(infoPackage info);
-
-    public infoPackage createInfoPackage(char action,Item item,int amount) {
-        return new infoPackage(action, item, amount);
-    }
-
-}
