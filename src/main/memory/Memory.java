e997fdfaa44720a6d136bf1e8cf8b7746c1f7cfc
senshi <senshi@10.1.10.233>
2020-07-27 14:43:27 -0400

    I hate ides now too

--- src/main/memory/Memory.java
+++ src/main/memory/Memory.java
@@ -1,6 +1,22 @@
-import java.util.maps.Map;
+package main.memory;
 
-public class Memory implements Map {
+import java.io.Serializable;
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Memory<K,V> extends AbstractMap<K,V> implements Map<K,V>, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 117578722165722509L;
+
+	@Override
+	public Set<Entry<K, V>> entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
     
     
 }
